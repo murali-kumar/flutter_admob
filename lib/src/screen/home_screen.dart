5a6c03565766b2050cdda40e9186f1255aceea03
murali kumar <muraliofficialid@gmail.com>
2021-09-13 16:55:45 +0530

    update

--- lib/src/screen/home_screen.dart
+++ lib/src/screen/home_screen.dart
@@ -10,6 +10,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Types of Google Ads'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ElevatedButton(
+              child: const Text('Banner Ad'),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              child: const Text('Interstitial Ad'),
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              child: const Text('Rewarded Ad'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
